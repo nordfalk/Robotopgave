b7dc423ce517445e5522321c427ce7d4b89039f7
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2020-10-27 14:30:43 +0100

    UI påbegyndt

--- app/src/main/java/dk/nordfalk/robotopgave/model/Model.java
+++ app/src/main/java/dk/nordfalk/robotopgave/model/Model.java
@@ -5,17 +5,15 @@ import java.util.ArrayList;
 import static dk.nordfalk.robotopgave.model.Retning.*;
 
 public class Model {
-    public ArrayList<Rum> rum = new ArrayList<>();
-    public ArrayList<Position> positioner = new ArrayList<>();
+    public ArrayList<Robot> starttilstande = new ArrayList<>();
     public ArrayList<String> programmer = new ArrayList<>();
 
 
     public Model() {
-        rum.add(new Rum(5,5));
-        rum.add(new Rum(1,1));
-
-        positioner.add(new Position(1,2, N));
-        positioner.add(new Position(0,0, E));
+        starttilstande.add(new Robot(new Rum(5,5), new Position(1,2, N)));
+        starttilstande.add(new Robot(new Rum(2,2), new Position(1,2, N)));
+        starttilstande.add(new Robot(new Rum(5,5), new Position(0,0, E)));
+        starttilstande.add(new Robot(new Rum(2,2), new Position(0,0, E)));
 
         programmer.add("RFRFFRFRF");
         programmer.add("RFLFFLRF");
